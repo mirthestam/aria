89afecd8ad33a93d634381273cc808be4ccf4994
mirthe <mirthe@nuvina.nl>
2026-02-11 11:08:41 +0100

    #41 Fixed credit box. Removed the wrap. Implemented a more expander.

--- src/Aria/Features/Browser/Album/CreditBox.cs
+++ src/Aria/Features/Browser/Album/CreditBox.cs
@@ -12,14 +12,14 @@ namespace Aria.Features.Browser.Album;
 [Template<AssemblyResource>("Aria.Features.Browser.Album.CreditBox.ui")]
 public partial class CreditBox
 {
-    [Connect("main-box")] private WrapBox _mainBox;
-    [Connect("composers-box")] private WrapBox _composersBox;
-    [Connect("arranged-box")] private WrapBox _arrangedBox;
-    [Connect("conductors-box")] private WrapBox _conductorsBox;
-    [Connect("performers-box")] private WrapBox _performersBox;
-    [Connect("solists-box")] private WrapBox _solistsBox;
-    
-    [Connect("main-label")] private Label _mainLabel;    
+    [Connect("main-box")] private Box _mainBox;
+    [Connect("composers-box")] private Box _composersBox;
+    [Connect("arranged-box")] private Box _arrangedBox;
+    [Connect("conductors-box")] private Box _conductorsBox;
+    [Connect("performers-box")] private Box _performersBox;
+    [Connect("solists-box")] private Box _solistsBox;
+
+    [Connect("main-label")] private Label _mainLabel;
     [Connect("composers-label")] private Label _composersLabel;
     [Connect("arranged-label")] private Label _arrangedLabel;
     [Connect("conductors-label")] private Label _conductorsLabel;
@@ -34,61 +34,81 @@ public partial class CreditBox
             Artist = a,
             Roles = ArtistRoles.Main
         }).ToList();
-        
-        FillArtistBox(_mainBox, _mainLabel, trackArtists, ArtistRoles.Main);        
+
+        FillArtistBox(_mainBox, _mainLabel, trackArtists, ArtistRoles.Main);
     }
-    
+
     public void UpdateTracksCredits(IList<TrackArtistInfo> artists)
     {
         // Do not remove Album Artists. In some tagging schemes, all artists are listed there.
         // Therefore, this is not a reliable source to determine whether they have already been shown.
         // A better approach would be to check whether the artists are shared across all tracks on the album.
         FillArtistBox(_composersBox, _composersLabel, artists, ArtistRoles.Composer);
-        FillArtistBox(_conductorsBox, _conductorsLabel,  artists, ArtistRoles.Conductor);
+        FillArtistBox(_conductorsBox, _conductorsLabel, artists, ArtistRoles.Conductor);
         FillArtistBox(_arrangedBox, _arrangedLabel, artists, ArtistRoles.Arranger);
-        FillArtistBox(_solistsBox, _solistsLabel, artists, ArtistRoles.Soloist);        
+        FillArtistBox(_solistsBox, _solistsLabel, artists, ArtistRoles.Soloist);
         FillArtistBox(_performersBox, _performersLabel, artists, ArtistRoles.Ensemble | ArtistRoles.Performer);
     }
-    
-    private void FillArtistBox(WrapBox box, Label label, IList<TrackArtistInfo> artists, ArtistRoles roleFilter)
+
+    private void FillArtistBox(Box container, Label label, IList<TrackArtistInfo> artists, ArtistRoles roleFilter)
     {
-        box.RemoveAll();
+        while (container.GetFirstChild() != null)
+        {
+            container.Remove(container.GetFirstChild()!);
+        }
+
         var filteredArtists = artists
             .OrderBy(a => a.Artist.Name)
             .ThenBy(a => (a.Roles & ArtistRoles.Composer) == 0) // In combined lists, prio composer over others
             .ThenBy(a => (a.Roles & ArtistRoles.Ensemble) == 0) // And then ensembles
             .Where(a => (a.Roles & roleFilter) != 0).ToList();
-        
+
         label.Visible = filteredArtists.Count > 0;
-        box.Visible = filteredArtists.Count > 0;
+        container.Visible = filteredArtists.Count > 0;
 
-        if (filteredArtists.Count <= 0) return;
-        
-        for (var i = 0; i < filteredArtists.Count; i++)
+        const int moreThreshold = 3;
+        const int moreMinimum = 3;
+
+        var added = 0;
+        foreach (var artist in filteredArtists)
         {
-            var artist = filteredArtists[i];
-            var isLastArtist = i == filteredArtists.Count - 1;
-            var nameButton = CreateArtistButton(artist.Artist);
-            box.Append(nameButton);
+            added++;
 
-            if (artist.AdditionalInformation != null)
+            if (added == moreThreshold + 1 && filteredArtists.Count - moreThreshold > moreMinimum)
             {
-                var additionalInfoLabel = Label.New($"({artist.AdditionalInformation})");
-                additionalInfoLabel.AddCssClass("dimmed");
-                box.Append(additionalInfoLabel);
+                var parentContainer = container;
+                var expander = Expander.New("More (" + (filteredArtists.Count - moreThreshold) + ")");
+                container = New(Orientation.Vertical, 2);
+                expander.SetChild(container);
+                parentContainer.Append(expander);
             }
 
-            if (!isLastArtist) box.Append(Label.New(","));
+            container.Append(CreateArtistBox(artist));
         }
     }
-    
+
+    private static Box CreateArtistBox(TrackArtistInfo artist)
+    {
+        var box = New(Orientation.Horizontal, 2);
+        box.Append(CreateArtistButton(artist.Artist));
+        if (artist.AdditionalInformation == null) return box;
+
+        var additionalInfoLabel = Label.New($"({artist.AdditionalInformation})");
+        additionalInfoLabel.AddCssClass("dimmed");
+        box.Append(additionalInfoLabel);
+
+        return box;
+    }
+
     private static Button CreateArtistButton(ArtistInfo artist)
     {
         // Format the button
         var displayText = artist.Name;
         var button = Button.NewWithLabel(displayText);
-        button.AddCssClass("link");
+        //button.AddCssClass("link");
+        button.AddCssClass("flat");
         button.AddCssClass("artist-link");
+        button.AddCssClass("accent");
 
         // Configure the action
         button.SetActionName($"{AppActions.Browser.Key}.{AppActions.Browser.ShowArtist.Action}");
@@ -96,5 +116,5 @@ public partial class CreditBox
         button.SetActionTargetValue(value);
 
         return button;
-    }    
+    }
 }
