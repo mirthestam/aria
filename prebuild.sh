08065d65db3c731e2405ee58dbd70c238706455e
mirthe <mirthe@nuvina.nl>
2026-02-21 22:40:47 +0100

    Upgraded to freedesktop 25.08

--- prebuild.sh
+++ prebuild.sh
@@ -4,7 +4,12 @@ set -eu
 # Generate nuget sources file
 [ ! -f ./flatpak/flatpak-dotnet-generator.py ] && wget https://raw.githubusercontent.com/flatpak/flatpak-builder-tools/master/dotnet/flatpak-dotnet-generator.py -O ./flatpak/flatpak-dotnet-generator.py
 echo "Updating nuget sources"
-python3 ./flatpak/flatpak-dotnet-generator.py --dotnet 10 --freedesktop 24.08 nuget-sources.json ./src/Aria.App/Aria.App.csproj
+python3 ./flatpak/flatpak-dotnet-generator.py \
+    --dotnet 10 \
+    --freedesktop 25.08 \
+    nuget-sources.json \
+    ./src/Aria.App/Aria.App.csproj
+
 
 # Check if gircore directory is not empty
 if [ -d ./lib/gircore/src/Libs/Adw-1/Internal ] && [ -n "$(ls -A ./lib/gircore/src/Libs/Adw-1/Internal 2>/dev/null)" ]; then
